41023f966ff11a10476d73ecf5552de26b9efaec
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-11 12:05:41 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("**Downloading the song by @SDbotsz**")
+    m.edit("**Downloading the song by @Venuja_Sadew**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Uploaded** by @SDbotsz.**'
+        rep = '**Uploaded** by @Venuja_Sadew.**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
