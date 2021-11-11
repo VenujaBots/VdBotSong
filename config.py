3b1cc055bdf71aae76b97f1a0621fbf0ffd1b9ea
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-11 12:02:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #SDBOTs <https://t.me/SDBOTs_Inifinity>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("7395896"))
+API_HASH = os.getenv("cd3998ddf318dad74d7c506731bc0abc")
+BOT_TOKEN = os.getenv("2128877912:AAFX7JIwiV5bdGF7Qfrf4T7w9vmt4wHPix8")
